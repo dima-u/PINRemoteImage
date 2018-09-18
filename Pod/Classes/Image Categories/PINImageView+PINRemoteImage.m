4bdb778a861e1b97753b92d5ef8014da05942c8d
Ulyanov dima <ulyanov_d_a@onyx-team.com>
2018-09-18 18:31:38 +0300

    possible fix of a popular crash on SL

--- Pod/Classes/Image Categories/PINImageView+PINRemoteImage.m
+++ Pod/Classes/Image Categories/PINImageView+PINRemoteImage.m
@@ -7,61 +7,93 @@
 //
 
 #import "PINImageView+PINRemoteImage.h"
+#import <objc/runtime.h>
+
+static void * PinRemoteVoidKey = &PinRemoteVoidKey;
+
+@interface WeakObjectContainer : NSObject
+@property (nonatomic, readonly, weak) id object;
+@end
+
+@implementation WeakObjectContainer
+- (instancetype) initWithObject:(id)object
+{
+    if (!(self = [super init]))
+        return nil;
+    
+    _object = object;
+    
+    return self;
+}
+@end
 
 @implementation PINImageView (PINRemoteImage)
 
-- (void)pin_setImageFromURL:(NSURL *)url
-{
-    [PINRemoteImageCategoryManager setImageOnView:self fromURL:url];
+- (id)weakObject {
+    return [objc_getAssociatedObject(self, PinRemoteVoidKey) object];
+}
+
+- (void)setWeakObject:(id)object {
+    objc_setAssociatedObject(self, &PinRemoteVoidKey, [[WeakObjectContainer alloc] initWithObject:object], OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 }
 
 - (void)pin_setImageFromURL:(NSURL *)url placeholderImage:(PINImage *)placeholderImage
 {
+    [self setWeakObject:[self superview]];
     [PINRemoteImageCategoryManager setImageOnView:self fromURL:url placeholderImage:placeholderImage];
 }
 
 - (void)pin_setImageFromURL:(NSURL *)url completion:(PINRemoteImageManagerImageCompletion)completion
 {
+    [self setWeakObject:[self superview]];
     [PINRemoteImageCategoryManager setImageOnView:self fromURL:url completion:completion];
 }
 
 - (void)pin_setImageFromURL:(NSURL *)url placeholderImage:(PINImage *)placeholderImage completion:(PINRemoteImageManagerImageCompletion)completion
 {
+    [self setWeakObject:[self superview]];
     [PINRemoteImageCategoryManager setImageOnView:self fromURL:url placeholderImage:placeholderImage completion:completion];
 }
 
 - (void)pin_setImageFromURL:(NSURL *)url processorKey:(NSString *)processorKey processor:(PINRemoteImageManagerImageProcessor)processor
 {
+    [self setWeakObject:[self superview]];
     [PINRemoteImageCategoryManager setImageOnView:self fromURL:url processorKey:processorKey processor:processor];
 }
 
 - (void)pin_setImageFromURL:(NSURL *)url placeholderImage:(PINImage *)placeholderImage processorKey:(NSString *)processorKey processor:(PINRemoteImageManagerImageProcessor)processor
 {
+    [self setWeakObject:[self superview]];
     [PINRemoteImageCategoryManager setImageOnView:self fromURL:url placeholderImage:placeholderImage processorKey:processorKey processor:processor];
 }
 
 - (void)pin_setImageFromURL:(NSURL *)url processorKey:(NSString *)processorKey processor:(PINRemoteImageManagerImageProcessor)processor completion:(PINRemoteImageManagerImageCompletion)completion
 {
+    [self setWeakObject:[self superview]];
     [PINRemoteImageCategoryManager setImageOnView:self fromURL:url processorKey:processorKey processor:processor completion:completion];
 }
 
 - (void)pin_setImageFromURL:(NSURL *)url placeholderImage:(PINImage *)placeholderImage processorKey:(NSString *)processorKey processor:(PINRemoteImageManagerImageProcessor)processor completion:(PINRemoteImageManagerImageCompletion)completion
 {
+    [self setWeakObject:[self superview]];
     [PINRemoteImageCategoryManager setImageOnView:self fromURLs:url?@[url]:nil placeholderImage:placeholderImage processorKey:processorKey processor:processor completion:completion];
 }
 
 - (void)pin_setImageFromURLs:(NSArray <NSURL *> *)urls
 {
+    [self setWeakObject:[self superview]];
     [PINRemoteImageCategoryManager setImageOnView:self fromURLs:urls];
 }
 
 - (void)pin_setImageFromURLs:(NSArray <NSURL *> *)urls placeholderImage:(PINImage *)placeholderImage
 {
+    [self setWeakObject:[self superview]];
     [PINRemoteImageCategoryManager setImageOnView:self fromURLs:urls placeholderImage:placeholderImage];
 }
 
 - (void)pin_setImageFromURLs:(NSArray <NSURL *> *)urls placeholderImage:(PINImage *)placeholderImage completion:(PINRemoteImageManagerImageCompletion)completion
 {
+    [self setWeakObject:[self superview]];
     [PINRemoteImageCategoryManager setImageOnView:self fromURLs:urls placeholderImage:placeholderImage completion:completion];
 }
 
@@ -97,9 +129,14 @@
 
 - (void)pin_updateUIWithImage:(PINImage *)image animatedImage:(FLAnimatedImage *)animatedImage
 {
+#if PIN_TARGET_IOS
+    if ([self weakObject] == nil){
+        return;
+    }
+#endif
     if (image) {
         self.image = image;
-
+        
 #if PIN_TARGET_IOS
         [self setNeedsLayout];
 #elif PIN_TARGET_MAC
@@ -125,3 +162,4 @@
 }
 
 @end
+
